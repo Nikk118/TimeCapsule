731f6a13661c4b0c71d244d3b97be4a1fd6cc688
nikhil <nikhilyagik2004@gmail.com>
2025-04-16 17:01:34 +0530

    users functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Routes,Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
@@ -7,23 +7,30 @@ import Footer from './components/Footer'
 import Login from './pages/Login'
 import Signup from './pages/signup'
 import Home from './pages/Home'
+import {ToastContainer} from "react-toastify"
+import { authStore } from './store/authStore'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
  
+
+  const {authUser,getUser}=authStore()
+
+  useEffect(()=>{
+    getUser()
+  },[getUser])
 
     return (
       <div className="m-0 p-0 w-screen h-screen">
        <Navbar/>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
-        <Route path='login' element={<Login/>}/>
-        <Route path='signup' element={<Signup/>}/>
-        <Route path='home' element={<Home/>}/>
+        <Route path='login' element={authUser?<Home/>:<Login/>}/>
+        <Route path='signup' element={authUser?<Home/>:<Signup/>}/>
+        <Route path='home' element={authUser?<Home/>:<Login/>}/>
       </Routes>
        <Footer/>
+       <ToastContainer position="top-right" autoClose={3000} />
       </div>
     );
   }
