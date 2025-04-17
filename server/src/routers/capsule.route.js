a4978a4a925ac5a567799d7a28fbf0427ef2c1f6
nikhil <nikhilyagik2004@gmail.com>
2025-04-17 18:03:25 +0530

    change

--- server/src/routers/capsule.route.js
+++ server/src/routers/capsule.route.js
@@ -5,7 +5,18 @@ import { createCapsule ,getUserCapsule} from "../controllers/capsule.controller.
 
 const router = Router()
 
-router.route("/createCapsule").post(verifyJwt,upload.array("media",5),createCapsule);
+router.route('/createCapsule')
+  .post(
+    verifyJwt,
+    upload.array('media', 5),  // file upload middleware
+    (err, req, res, next) => {
+      if (err) {
+        return res.status(400).json({ message: err.message });
+      }
+      next(); // Continue if no error
+    },
+    createCapsule  // Capsule creation logic
+  );
 
 router.route("/getUserCapsule").get(verifyJwt,getUserCapsule)
 
