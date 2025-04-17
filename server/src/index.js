7192904596d7f78e8814ce3bb047a4f583ea8296
nikhil <nikhilyagik2004@gmail.com>
2025-04-17 17:44:26 +0530

    again minor change

--- server/src/index.js
+++ server/src/index.js
@@ -42,7 +42,7 @@ import { startDeliveryCron } from './cron/sendCapsules.js';
 startDeliveryCron();
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(--dirname,"../client/dist")));
+    app.use(express.static(path.join(__dirname,"../client/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../client","dist","index.html"));
     })
